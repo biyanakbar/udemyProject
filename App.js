b7a3a399077e090291a97f745a392e3d61425d3d
Akbarers <biyan96akbar@gmail.com>
2019-08-05 03:43:22 +0700

    35. Rendering Lists Correctly
    FlatList

--- App.js
+++ App.js
@@ -17,16 +17,19 @@ export default class App extends Component {
   submitAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random().toString(),
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeleteHandler = index => {
+  placeDeleteHandler = key => {
     this.setState(prevState => {
       return {
-        places : prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter((place) => {
+          return place.key !== key;
         })
       }
     })
