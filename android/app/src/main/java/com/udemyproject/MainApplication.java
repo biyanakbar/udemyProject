fbde39d36cd0c2e3d427aaef713bc1cd7daacf9f
Akbarers <biyan96akbar@gmail.com>
2019-08-18 22:32:01 +0700

    Linking Libraries on Android

--- android/app/src/main/java/com/udemyproject/MainApplication.java
+++ android/app/src/main/java/com/udemyproject/MainApplication.java
@@ -11,6 +11,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
